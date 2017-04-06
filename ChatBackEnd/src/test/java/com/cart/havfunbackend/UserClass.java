2b7ce68f5d93d5213b7d5a5ce9edc0345722141b
AnantheeRaj <anantheeraj@gmail.com>
2017-04-06 14:32:05 +0530

    testcase changes

--- ChatBackEnd/src/test/java/com/cart/havfunbackend/UserClass.java
+++ ChatBackEnd/src/test/java/com/cart/havfunbackend/UserClass.java
@@ -1,25 +1,26 @@
 package com.cart.havfunbackend;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 import com.cart.dao.UserDao;
 import com.cart.dao.UserDaoImpl;
 import com.cart.model.User;
 
-
-public class DemoTest {
+public class UserClass {
+	    
 	
+    @Autowired
+    static UserDao ud;
+	    
 	public static void main(String[] args) {
-		 UserDao userdao;
-		//AnnotationConfigWebApplicationContext ct=new AnnotationConfigWebApplicationContext();
-		 AnnotationConfigApplicationContext ct = new AnnotationConfigApplicationContext();
-		 ct.scan("com.cart.config");
-		 ct.refresh();
-		 userdao=(UserDao)ct.getBean("UserDaoImpl");
-		 User user=new User();
-		 user.setUserId("123");
-		 user.setStatus('N');
+		/*AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+	    ctx.scan("com.cart.config");
+	    ctx.refresh();*/
+	    ud = new UserDaoImpl();
+		User user=new User();
+		user.setUserId("123");
+		user.setStatus('N');
 		 user.setRole("STUDENT");
 		 user.setReason("");
 		 user.setPhoneNo("8798765434");
@@ -30,9 +31,10 @@ public class DemoTest {
 		 user.setErrorCode("404");
 		 user.setEmailId("demo@gmail.com");
 		 user.setAddress("chennai");
-		 userdao.saveUser(user);
-			 
-			 
-		 }
+		if(ud.saveUser(user))
+			System.out.println("saved");
+		else
+			System.out.println("not saved");
 	}
-	
+
+	}
